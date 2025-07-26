6b2bf36e3248308a7d44742f9bb64d520e007daf
Caique Vidal <caique.v.freitas@ba.estudante.senai.br>
2025-07-25 21:59:20 -0300

    style: Finalização do css da página de Inicio

--- front/src/views/inicio.vue
+++ front/src/views/inicio.vue
@@ -3,11 +3,17 @@
 </script>
 
 <template>
-    <div class="bg-black w-screen h-screen flex justify-center items-center flex-col gap-8">
+    <div class="bg-black w-screen h-screen">
+        <div class="text-center">
+            <h1 class="text-white text-7xl p-20">Meus animes</h1>
+        </div>
+        <div class="flex justify-center items-center flex-col gap-8">
             <ButtonLinkPages link="#/ListadeAnimes" title="Lista"/>
             <ButtonLinkPages link="#/CadastrodeAnimes" title="Cadastrar"/>
             <ButtonLinkPages link="#/EditarAnimes" title="Editar"/>
-    </div>  
+        </div>  
+    </div>
+
 </template>
 
 
